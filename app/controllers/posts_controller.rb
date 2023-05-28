cd182adee1c03573a30f5abd94cfdd99c8b26b07
Daniel Neel <dneel10@gmail.com>
2023-05-27 23:24:33 -0400

    update view to show post authoer only when logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.create(post_params)
 
     if @post.save
       redirect_to root_path
@@ -22,6 +22,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body, :user_id)
+    params.require(:post).permit(:title, :body)
   end
 end
